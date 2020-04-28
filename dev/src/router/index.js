ac1ecb418856b453b5147799f79eedc6b7e531ff
Giuseppe Caso <gcaso@Giuseppes-MacBook-Pro.local>
2020-04-28 23:02:10 +0200

    define wrap structure, base fonts and others colors

--- dev/src/router/index.js
+++ dev/src/router/index.js
@@ -6,15 +6,10 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'Index',
+    components: {
+      default: () => import(/* webpackChunkName: "mainWrap" */ '@/views/main-wrap/MainWrap.vue'),
+    },
   },
 ];
 
